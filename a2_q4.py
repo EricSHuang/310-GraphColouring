f77c4228912d44baf6bf3ae6db94f1c4d6c605a1
Eric Huang <ericshuang23@gmail.com>
2019-06-05 00:04:40 -0700

    Q3 and Q4
    -Incorporated Q3 into function "run_q3"
    -Copied Q3 to Q4

--- a2_q4.py
+++ a2_q4.py
@@ -8,16 +8,16 @@ import time
 
 
 def run_q3():
-    graphs = [rand_graph(30, 0.1), rand_graph(30, 0.2), rand_graph(30, 0.3),
-              rand_graph(30, 0.4), rand_graph(30, 0.5)]
+    graphs = [rand_graph(100, 0.1), rand_graph(100, 0.2), rand_graph(100, 0.3),
+              rand_graph(100, 0.4), rand_graph(100, 0.5)]
     #graphs = [{0: [1, 2], 1: [0], 2: [0], 3: []}]    #test graph
 
-    numRounds = 1     #set to 5
+    numRounds = 2     #set to 5
     for i in range(0, numRounds):
         print("------------------")
         print("*****Round %d*****" %(i+1))
         print("------------------")
-        for j in range(0, len(graphs)): #set to len(graphs)
+        for j in range(0, len(graphs)):
             currGraph = graphs[j]
             startTime = time.time()
             #print(currGraph)
@@ -30,28 +30,25 @@ def run_q3():
             domains = {}
             #each node could be in any group from 0 to numVars-1
             for k in range(0, len(currGraph)):
-                #domains[k] = list(range(0, len(currGraph)))
-                domains[k] = list(range(0, 1))
-            d2 = [0,1]
+                domains[k] = list(range(0, len(currGraph)))
 
             neighbors = currGraph
             constraint = different_values_constraint
 
-            csp = CSP(variables, domains, neighbors, constraint)
-            #csp = MapColoringCSP(d2, neighbors)
+            #csp = CSP(variables, domains, neighbors, constraint)
+            csp = MapColoringCSP(domains, neighbors)
             #print(csp.variables)
             #print("neighbors: ", csp.neighbors)
-            print("pre ac3: ", csp.domains)
+            #print("pre ac3: ", csp.domains)
             #print("constraints: ", csp.constraints)
             consistent = AC3(csp)
             if (not consistent):
                 print("GRAPH %d WAS NOT CONSISTENT" %(j))
                 break;
-            print("post ac3: ", csp.domains)
+            #print("post ac3: ", csp.domains)
 
             """ANSWER AND REPORTS"""
-            ans = backtracking_search(csp)
-            print(ans)
+            ans = min_conflicts(csp)
             print("-----Probability %0.1f Graph -----" %((j * 0.1) + 0.1))
 
             #Running Time
